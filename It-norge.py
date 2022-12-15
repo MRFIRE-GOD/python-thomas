6198e11774e805da95f34ce5da12ccf7c3122eeb
MRFIRE-GOD <ttesfayohans22@gmail.com>
2022-12-15 10:30:48 +0100

    EDRING

--- It-norge.py
+++ It-norge.py
@@ -1,6 +1,11 @@
 <<<<<<< Updated upstream
 import platform
+<<<<<<< Updated upstream
 import shutil
+=======
+import socket
+import subprocess
+>>>>>>> Stashed changes
 import psutil
 
 hvaErOS=platform.architecture()
@@ -19,6 +24,53 @@ print(hvaErprosesor)
 print("----------------------------------------------")
 
 #lagring plass på pc-en din
+<<<<<<< Updated upstream
 =======
 from platform import system, node, release, version, machine, processor 
+>>>>>>> Stashed changes
+=======
+def lagring():
+    total = int()
+    used = int()
+    free = int()
+    for disk in psutil.disk_partitions():
+        if disk.fstype:
+            total += int(psutil.disk_usage(disk.mountpoint).total)
+            used += int(psutil.disk_usage(disk.mountpoint).used)
+            free += int(psutil.disk_usage(disk.mountpoint).free)
+
+            print(f"Du hadde : {round(total / (1024.0 ** 3), 4)} Giga")
+            print(f"Hvor mye du har used : {round(used / (1024.0 ** 3), 4)} Giga")
+            print(f"hvor mye du har igjen : {round(free / (1024.0 **3),4)} Giga")
+
+
+
+#Ip address
+def ip_address():
+    hostname = socket.gethostname()
+    ip = socket.gethostbyname(hostname)
+    print("pc-navnet er :" + hostname)
+    print("Ip address er : " + ip)
+
+
+
+#programmer du har
+def program():
+
+    Data = subprocess.check_output(['wmic', 'product', 'get', 'name'])
+    a = str(Data)
+
+    try:
+        for i in range(len(a)):
+            print("Du har: ", (a.split("\\r\\r\\n")[6:][i]))
+
+    except IndexError as e:
+        print("Ferdig se opp")
+
+
+
+lagring()
+ip_address()
+program()
+#this com.. is just to get more in git hub omh
 >>>>>>> Stashed changes
