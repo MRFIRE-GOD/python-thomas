83d9fac4c275a6a3496e798c8286c3f829541ce3
MRFIRE-GOD <ttesfayohans22@gmail.com>
2022-12-15 10:10:47 +0100

    trying

--- test.py
+++ test.py
@@ -1,14 +1,3 @@
-import subprocess
-def program():
-
-    Data = subprocess.check_output(['wmic', 'product', 'get', 'name'])
-    a = str(Data)
-
-    try:
-        for i in range(len(a)):
-            print("Du har: ", (a.split("\\r\\r\\n")[6:][i]))
-
-    except IndexError as e:
-        print("Ferdig se opp")
-print("---------------------------------------------")
-program()
+import platform
+def get_platform(system, enhten, pc, vetsjon, Maskin, prosesor):
+    print(2f(system){my_system.system})
--- test2.py
+++ test2.py
@@ -1,14 +1,5 @@
-import subprocess
-def program():
-
-    Data = subprocess.check_output(['wmic', 'product', 'get', 'name'])
-    a = str(Data)
-
-    try:
-        for i in range(len(a)):
-            print("Du har: ", (a.split("\\r\\r\\n")[6:][i]))
-
-    except IndexError as e:
-        print("Ferdig se opp")
-print("---------------------------------------------")
-program()
+import platform
+hvaErOS=platform.architecture()
+print(hvaErOS)
+hvaEr=platform.node()
+print(hvaEr)
